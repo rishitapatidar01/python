7181115ef6658b1c47b74d890b1da1e1acf3af6d
rishitapatidar01 <rishitapatidar2003@gmail.com>
2022-12-13 19:31:37 +0530

    remove

--- count.py
+++
@@ -1,5 +0,0 @@
-str2=input("enter your string")
-count=0
-for i in str2:
-	count=count(i)
-print(count)
--- fact2.py
+++
@@ -1,9 +0,0 @@
-i=int(input("enter the fact"))
-fact=1
-if i==0 or i==1:
-	print("factorial of {} is {}". format(i,fact))
-else:
-	for j in range(i,0,-1):
-		fact=fact*j
-		print(fact*i)
-	print("factorial of{} is {}".format(i,fact))
