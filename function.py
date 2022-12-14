e9fef55fc91b1f9ac287915e519db4802c64c244
rishitapatidar01 <rishitapatidar2003@gmail.com>
2022-12-14 19:21:49 +0530

    function program

---
+++ function.py
@@ -0,0 +1,12 @@
+def rect_area():
+	length=int(input("enter length"))
+	breadth=int(input("entr the breadth"))
+	area=length*breadth
+	print("area of rectangle=",area)
+"""rect_area()
+rect_area()
+rect_area()
+rect_area()"""
+for i in range(4):
+	rect_area()
+	
